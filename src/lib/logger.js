29453b2ab59ed41b088d6461d7f9ad57fbe898c9
Pablo Zagni <pablozagniyahoo.com.ar>
2025-06-18 10:18:02 -0300

    log fix

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,4 +1,12 @@
 const { createLogger, format, transports } = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+// Asegura que la carpeta logs exista
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+}
 
 const logger = createLogger({
     level: 'info',
@@ -7,7 +15,7 @@ const logger = createLogger({
         format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
     ),
     transports: [
-        new transports.File({ filename: 'whatsapp-app.log' }),
+        new transports.File({ filename: path.join(logDir, 'whatsapp-app.log') }),
         new transports.Console()
     ]
 });
